18edf79ef2f36bdcf2ac64633fd837486bc027c5
JoelClemence <jclemence@companieshouse.gov.uk>
2024-06-13 13:56:05 +0100

    shfmt

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ log() {
   esac
 
   case "${log_level}" in
-  DEBUG) log_colour="";;
+  DEBUG) log_colour="" ;;
   INFO) log_colour="${F_FG_GREEN}" ;;
   WARN*) log_colour="${F_FG_YELLOW}" ;;
   ERROR) log_colour="${F_FG_RED}" ;;
@@ -89,10 +89,10 @@ log() {
 validate_version() {
   log DEBUG 'validating version'
 
-{
+  {
     jq -r '.[] | .name' "${releases_file}"
     printf -- '%s' "$?" >"${temp_d}"/jq_status
-} |
+  } |
     {
       grep -q "${VERSION}"
       printf -- '%s' "$?" >"${temp_d}"/grep_status
@@ -109,84 +109,84 @@ validate_version() {
 # operating system is unsupported by the CLI (i.e. missing compiled CLI
 # artifacts). Panics if not OSX or Linux compatible
 determine_operating_system() {
-    log DEBUG 'determining operating system'
-    uname_out="$(uname -a)"
-    case "${uname_out}" in
-    *Microsoft*) OS="linux" ;;  #must be first since Windows subsystem for linux will have Linux in the name too
-    *microsoft*) OS="linux" ;; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
-    Linux*) OS="linux" ;;
-    Darwin*) OS="darwin" ;; # Apple Mac
-    *)
-        panic "Unsupported operating system. Must be Linux or Mac."
+  log DEBUG 'determining operating system'
+  uname_out="$(uname -a)"
+  case "${uname_out}" in
+  *Microsoft*) OS="linux" ;; #must be first since Windows subsystem for linux will have Linux in the name too
+  *microsoft*) OS="linux" ;; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
+  Linux*) OS="linux" ;;
+  Darwin*) OS="darwin" ;; # Apple Mac
+  *)
+    panic "Unsupported operating system. Must be Linux or Mac."
     ;;
-    esac
+  esac
 }
 
 # Sets the variable CHIPSET with the name of the current chipset architecture.
 # Panics if the chipset is not arm64 or x86
 determine_chipset() {
-    log DEBUG 'determining chipset'
-    uname_out="$(uname -m)"
+  log DEBUG 'determining chipset'
+  uname_out="$(uname -m)"
 
-    case "${uname_out}" in
-        aarch64|arm64) CHIPSET=arm64;;
-        x86_64*) CHIPSET=x64;;
-        *)
-            panic "Unsupported chipset. Supports: arm64 or x64 architectures."
-            ;;
-    esac
+  case "${uname_out}" in
+  aarch64 | arm64) CHIPSET=arm64 ;;
+  x86_64*) CHIPSET=x64 ;;
+  *)
+    panic "Unsupported chipset. Supports: arm64 or x64 architectures."
+    ;;
+  esac
 }
 
 # Downloads the releases to local file
 download_releases() {
-    curl  -s -L \
-        -o "${releases_file}" \
-        -H 'Accept: application/vnd.github+json' \
-        https://api.github.com/repos/companieshouse/chs-dev/releases
+  curl -s -L \
+    -o "${releases_file}" \
+    -H 'Accept: application/vnd.github+json' \
+    https://api.github.com/repos/companieshouse/chs-dev/releases
 }
 
 # Interrogates the release to determine the download URL for the CLI tarball
 determine_download_url() {
-    release_file="${temp_d}"/release.json
+  release_file="${temp_d}"/release.json
 
-    if [ "${VERSION}" = "${DEFAULT_VERSION}" ]; then
-        log DEBUG 'Fetching latest release'
-        curl -s -L -H \
-            'Accept: application/vnd.github+json' \
-            https://api.github.com/repos/companieshouse/chs-dev/releases/latest > "${release_file}"
-    else
-        log DEBUG "Looking up version ${VERSION}"
-        jq \
-            --arg version "${VERSION}" \
-            '.[] | select(.name == $version)' \
-            "${releases_file}" > "${release_file}"
-    fi
+  if [ "${VERSION}" = "${DEFAULT_VERSION}" ]; then
+    log DEBUG 'Fetching latest release'
+    curl -s -L -H \
+      'Accept: application/vnd.github+json' \
+      https://api.github.com/repos/companieshouse/chs-dev/releases/latest >"${release_file}"
+  else
+    log DEBUG "Looking up version ${VERSION}"
+    jq \
+      --arg version "${VERSION}" \
+      '.[] | select(.name == $version)' \
+      "${releases_file}" >"${release_file}"
+  fi
 
-    install_version="$(jq -r '.name' "${release_file}")"
-    log INFO "Installing version ${install_version}"
+  install_version="$(jq -r '.name' "${release_file}")"
+  log INFO "Installing version ${install_version}"
 
-    jq -r --arg expected_suffix "-${OS}-${CHIPSET}.tar.gz" \
-        '.assets[] | select(.name | endswith($expected_suffix)) | .browser_download_url' \
-        "${release_file}" > "${temp_d}"/download_url
+  jq -r --arg expected_suffix "-${OS}-${CHIPSET}.tar.gz" \
+    '.assets[] | select(.name | endswith($expected_suffix)) | .browser_download_url' \
+    "${release_file}" >"${temp_d}"/download_url
 }
 
 # Downloads the tarball to temporary directory
 download_cli_tarball() {
-    log DEBUG "Downloading - '${DOWNLOAD_URL}'"
+  log DEBUG "Downloading - '${DOWNLOAD_URL}'"
 
-    curl -s \
-        -o "${downloaded_cli_tar_file}" \
-        -L \
-        -H 'Accept: application/tar+gzip' \
-        "${DOWNLOAD_URL}"
+  curl -s \
+    -o "${downloaded_cli_tar_file}" \
+    -L \
+    -H 'Accept: application/tar+gzip' \
+    "${DOWNLOAD_URL}"
 }
 
 # If Symlink directory is not found on path then output instructions on how to
 # add to path
 add_chs_dev_cli_to_path() {
-    if printf -- '%s' "${PATH}" | grep -qv -- "${SYMLINK_DIRECTORY}"; then
-        log WARN "Does not look like chs-dev will be on your path"
-        cat <<EOF
+  if printf -- '%s' "${PATH}" | grep -qv -- "${SYMLINK_DIRECTORY}"; then
+    log WARN "Does not look like chs-dev will be on your path"
+    cat <<EOF
 If you haven't installed via dev-env-setup you will need to modify your shell
 profile to add the following:
 
@@ -194,56 +194,56 @@ profile to add the following:
 
 This can be added to your ~/.bashrc or ~/.zshrc file (depending on your shell)
 EOF
-    fi
+  fi
 }
 
 # Given the installation directory, unzip the downloaded tarball and extract
 # to the installation directory and then create symlink to chs-dev binary
 install_cli_tarball() {
-    log DEBUG "installing tarball..."
-    return_status=0
+  log DEBUG "installing tarball..."
+  return_status=0
 
-    if [ ! -d "${INSTALLATION_DIRECTORY}" ]; then
-        log DEBUG "Creating ${INSTALLATION_DIRECTORY}"
-        mkdir -p "${INSTALLATION_DIRECTORY}" || return_status=$?
+  if [ ! -d "${INSTALLATION_DIRECTORY}" ]; then
+    log DEBUG "Creating ${INSTALLATION_DIRECTORY}"
+    mkdir -p "${INSTALLATION_DIRECTORY}" || return_status=$?
+  fi
+
+  if [ "${return_status}" -eq 0 ]; then
+    log DEBUG "Unzipping tarball..."
+
+    tar -xf "${downloaded_cli_tar_file}" \
+      -C "${temp_d}" || return_status=$?
+  fi
+
+  if [ "${return_status}" -eq 0 ]; then
+    (
+      cd "${temp_d}"/chs-dev/ || exit 1
+      tar c .
+    ) | (
+      cd "${INSTALLATION_DIRECTORY}" || exit 1
+      tar xf -
+    ) || return_status=$?
+
+    if [ "${return_status}" -ne 0 ]; then
+      panic 'Unable to install CLI to destination'
+    fi
+  fi
+
+  if [ -z "${NO_SYMLINK}" ]; then
+    log DEBUG "Installing symlink"
+    mkdir -p "${SYMLINK_DIRECTORY}" || return_status=$?
+
+    if [ "${return_status}" -eq 0 ]; then
+      ln -s "${INSTALLATION_DIRECTORY}"/bin/chs-dev "${SYMLINK_DIRECTORY}"/chs-dev || return_status=$?
     fi
 
     if [ "${return_status}" -eq 0 ]; then
-        log DEBUG "Unzipping tarball..."
-
-        tar -xf "${downloaded_cli_tar_file}" \
-            -C "${temp_d}" || return_status=$?
+      add_chs_dev_cli_to_path || return_status=$?
     fi
-
-    if [ "${return_status}" -eq 0 ]; then
-        (
-            cd "${temp_d}"/chs-dev/ || exit 1
-            tar c .
-        ) | (
-            cd "${INSTALLATION_DIRECTORY}" || exit 1
-            tar xf -
-        ) || return_status=$?
-
-        if [ "${return_status}" -ne 0 ]; then
-            panic 'Unable to install CLI to destination'
-        fi
-    fi
-
-    if [ -z "${NO_SYMLINK}" ]; then
-        log DEBUG "Installing symlink"
-        mkdir -p "${SYMLINK_DIRECTORY}" || return_status=$?
-
-        if [ "${return_status}" -eq 0 ]; then
-            ln -s "${INSTALLATION_DIRECTORY}"/bin/chs-dev "${SYMLINK_DIRECTORY}"/chs-dev || return_status=$?
-        fi
-
-        if [ "${return_status}" -eq 0 ]; then
-            add_chs_dev_cli_to_path || return_status=$?
-        fi
-    else
-        log WARN "Skipping symlink installation"
-        printf -- '\n'
-        cat <<'EOF'
+  else
+    log WARN "Skipping symlink installation"
+    printf -- '\n'
+    cat <<'EOF'
 It is unlikely that chs-dev will be on your path, therefore you will have to
 add it to your shell profile. Your options:
 
@@ -252,9 +252,9 @@ add it to your shell profile. Your options:
 * Create a function/alias in your shell profile to the absolute path to the
   `bin/chs-dev` executable in your local installation directory
 EOF
-    fi
+  fi
 
-    return "${return_status}"
+  return "${return_status}"
 }
 
 # Function which prompts user to confirm they are happy to continue
@@ -264,102 +264,102 @@ EOF
 # 0  - Action confirmed
 # 1  - User has explicitly not confirmed the action
 user_confirm_action() {
-    prompt="${1:?prompt required}"
+  prompt="${1:?prompt required}"
 
-    if [ -z "${FORCE}" ]; then
-        printf -- '%s\n-- To continue press y\n' "${prompt}"
-        # explicitly read from tty since stdin may be a pipe
-        read -r user_confirmation < /dev/tty
+  if [ -z "${FORCE}" ]; then
+    printf -- '%s\n-- To continue press y\n' "${prompt}"
+    # explicitly read from tty since stdin may be a pipe
+    read -r user_confirmation </dev/tty
 
-        case "${user_confirmation}" in
-            [Yy]) return 0;;
-            *)
-                log WARN "User not entered 'y' bailing out..."
-                return 1
-                ;;
-        esac
-    else
-        log DEBUG 'In force mode autoconfirming'
-        return 0
-    fi
+    case "${user_confirmation}" in
+    [Yy]) return 0 ;;
+    *)
+      log WARN "User not entered 'y' bailing out..."
+      return 1
+      ;;
+    esac
+  else
+    log DEBUG 'In force mode autoconfirming'
+    return 0
+  fi
 
 }
 
 # Runs through the steps of installing the chs-dev cli
 install() {
-    # Try to find chs-dev and if present offer to uninstall it before
-    # installing the latest version
-    if [ -L "${SYMLINK_DIRECTORY}"/chs-dev ] ||
-        [ -d "${INSTALLATION_DIRECTORY}" ];  then
-        chs_dev_version_installed=$(chs-dev --version || : "")
+  # Try to find chs-dev and if present offer to uninstall it before
+  # installing the latest version
+  if [ -L "${SYMLINK_DIRECTORY}"/chs-dev ] ||
+    [ -d "${INSTALLATION_DIRECTORY}" ]; then
+    chs_dev_version_installed=$(chs-dev --version || : "")
 
-        if [ -n "${chs_dev_version_installed}" ]; then
-            log WARN "chs-dev (version ${chs_dev_version_installed}) already installed"
-        else
-            log WARN "chs-dev already installed"
-        fi
-
-        if user_confirm_action "Do you want to reinstall chs-dev?"; then
-            FORCE=1 uninstall
-        else
-            panic "chs-dev cli not installed"
-        fi
-    fi
-
-    # Download the most recent releases and validate the version
-    download_releases
-
-    if [ ! "${VERSION}" = "${DEFAULT_VERSION}" ]; then
-        if ! validate_version; then
-            panic 'Could not install chs-dev'
-        fi
-    fi
-
-    # Set the required information in order to download tarball
-    determine_operating_system
-
-    determine_chipset
-
-    # captured in variable so no way of doing ! cmd
-    # shellcheck disable=SC2181
-    if ! determine_download_url; then
-        panic "Could not find download for release specified."
-    fi
-
-    # Download tarball
-    DOWNLOAD_URL="$(cat "${temp_d}"/download_url)"
-
-    log INFO 'Downloading CLI tarball. Will take a few moments...'
-
-    if ! download_cli_tarball; then
-        panic "Could not download release tarball."
-    fi
-
-    # Install tarball
-    if install_cli_tarball; then
-        log INFO 'chs-dev CLI installed successfully.'
+    if [ -n "${chs_dev_version_installed}" ]; then
+      log WARN "chs-dev (version ${chs_dev_version_installed}) already installed"
     else
-        panic "CLI not installed"
+      log WARN "chs-dev already installed"
     fi
+
+    if user_confirm_action "Do you want to reinstall chs-dev?"; then
+      FORCE=1 uninstall
+    else
+      panic "chs-dev cli not installed"
+    fi
+  fi
+
+  # Download the most recent releases and validate the version
+  download_releases
+
+  if [ ! "${VERSION}" = "${DEFAULT_VERSION}" ]; then
+    if ! validate_version; then
+      panic 'Could not install chs-dev'
+    fi
+  fi
+
+  # Set the required information in order to download tarball
+  determine_operating_system
+
+  determine_chipset
+
+  # captured in variable so no way of doing ! cmd
+  # shellcheck disable=SC2181
+  if ! determine_download_url; then
+    panic "Could not find download for release specified."
+  fi
+
+  # Download tarball
+  DOWNLOAD_URL="$(cat "${temp_d}"/download_url)"
+
+  log INFO 'Downloading CLI tarball. Will take a few moments...'
+
+  if ! download_cli_tarball; then
+    panic "Could not download release tarball."
+  fi
+
+  # Install tarball
+  if install_cli_tarball; then
+    log INFO 'chs-dev CLI installed successfully.'
+  else
+    panic "CLI not installed"
+  fi
 }
 
 # Uninstalls the installed CLI
 uninstall() {
-    log INFO "Uninstalling CLI tool"
+  log INFO "Uninstalling CLI tool"
 
-    # Get user to confirm they are happy to uninstall chs-dev CLI
-    if ! user_confirm_action "This will uninstall chs-dev CLI do you want to continue?"; then
-        panic "chs-dev cli not uninstalled"
-    fi
+  # Get user to confirm they are happy to uninstall chs-dev CLI
+  if ! user_confirm_action "This will uninstall chs-dev CLI do you want to continue?"; then
+    panic "chs-dev cli not uninstalled"
+  fi
 
-    # Remove symlink and the chs-dev directory
-    log DEBUG "Removing symlink"
-    rm -f "${SYMLINK_DIRECTORY}"/chs-dev
+  # Remove symlink and the chs-dev directory
+  log DEBUG "Removing symlink"
+  rm -f "${SYMLINK_DIRECTORY}"/chs-dev
 
-    log DEBUG "Removing CLI files"
-    rm -rf "${INSTALLATION_DIRECTORY}"
+  log DEBUG "Removing CLI files"
+  rm -rf "${INSTALLATION_DIRECTORY}"
 
-    log INFO "chs-dev CLI uninstalled successfully"
+  log INFO "chs-dev CLI uninstalled successfully"
 }
 
 if ! command -v jq >/dev/null 2>&1; then
@@ -395,8 +395,8 @@ shift "$((OPTIND - 1))"
 COMMAND="${1-"${DEFAULT_COMMAND}"}"
 
 case "${COMMAND}" in
-    install|uninstall) ;;
-    *) panic "Command can only be install or uninstall" ;;
+install | uninstall) ;;
+*) panic "Command can only be install or uninstall" ;;
 esac
 
 # Set the values of the variables to the defaults if unset
