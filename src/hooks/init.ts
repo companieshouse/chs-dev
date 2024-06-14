7123f8db60588662c31ef2692b3574b40149112f
JoelClemence <jclemence@companieshouse.gov.uk>
2024-06-14 12:16:07 +0100

    Refactor to extract functions

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,64 +1,28 @@
 import { Hook } from "@oclif/config";
 
-import { minVersion, satisfies, gt, parse } from "semver";
-import { engines } from "../../package.json";
+import { SemVer, gt, parse } from "semver";
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import { getLatestReleaseVersion } from "../helpers/latest-release.js";
 import { join } from "path";
 
 export const hook: Hook<"init"> = async function (options) {
-    let checkVersion: boolean = "CHS_DEV_CHECK_VERSION" in process.env;
-
-    const executionTime = Date.now();
 
     const runTimeFile = join(options.config.dataDir, "last-version-run-time");
+    const executionTime = Date.now();
+    const runThresholdDays = options.config.pjson["chs-dev"]["version-check-schedule"]["number-of-days"];
 
-    if (!existsSync(options.config.dataDir)) {
+    const dataDirectoryExists = existsSync(options.config.dataDir);
+
+    let checkVersion: boolean = "CHS_DEV_CHECK_VERSION" in process.env || !(
+        dataDirectoryExists && existsSync(runTimeFile) && hasBeenRunRecently(executionTime, runThresholdDays, runTimeFile)
+    );
+
+    if (!dataDirectoryExists) {
         mkdirSync(options.config.dataDir, { recursive: true });
-
-        checkVersion = true;
-    } else if (!existsSync(runTimeFile)) {
-        checkVersion = true;
-    } else {
-        const lastRun = readFileSync(runTimeFile).toString("utf8");
-
-        const difference = executionTime - Date.parse(lastRun);
-
-        const numberOfDaysLapsed = Math.floor(
-            difference / (60 * 60 * 24 * 1000)
-        );
-
-        if (numberOfDaysLapsed >= parseInt(options.config.pjson["chs-dev"]["version-check-schedule"]["number-of-days"])) {
-            checkVersion = true;
-        }
     }
 
     if (checkVersion) {
-        const latestVersion = await getLatestReleaseVersion();
-
-        if (gt(latestVersion, options.config.version)) {
-            const latestVersionSemver = parse(latestVersion);
-            const versionSemver = parse(options.config.version);
-            let versionDifference = "";
-
-            if (latestVersionSemver?.major !== versionSemver?.major) {
-                versionDifference = " major";
-            } else if (latestVersionSemver?.minor !== versionSemver?.minor) {
-                versionDifference = " minor";
-            }
-
-            console.log(
-                "=".repeat(80)
-            );
-
-            console.log(
-                `\n📣 There is a newer${versionDifference} version (${latestVersion}) available (current version: ${options.config.version})\n`
-            );
-
-            console.log(
-                "=".repeat(80)
-            );
-        }
+        await checkCurrentVersionOfCliIsLatest(options);
 
         writeFileSync(
             runTimeFile,
@@ -67,3 +31,55 @@ export const hook: Hook<"init"> = async function (options) {
     }
 
 };
+
+const hasBeenRunRecently:  (executionTime: number, dayThreshold: string, runTimeFile: string) => boolean = (executionTime: number, dayThreshold: string, runTimeFile: string) => {
+    let runRecently: boolean = true;
+
+    const lastRun = readFileSync(runTimeFile).toString("utf8");
+
+    const difference = executionTime - Date.parse(lastRun);
+
+    const numberOfDaysLapsed = Math.floor(
+        difference / (60 * 60 * 24 * 1000)
+    );
+
+    if (numberOfDaysLapsed >= parseInt(dayThreshold)) {
+        runRecently = false;
+    }
+
+    return runRecently;
+}
+
+const checkCurrentVersionOfCliIsLatest = async (options) => {
+    const latestVersion = await getLatestReleaseVersion();
+    const currentVersion = options.config.version;
+
+    if (gt(latestVersion, currentVersion)) {
+        const latestVersionSemver = parse(latestVersion);
+        const versionSemver = parse(currentVersion);
+
+        logVersionDifference(latestVersionSemver, versionSemver);
+    }
+}
+
+const logVersionDifference = (latestVersionSemver: SemVer | null, versionSemver: SemVer | null) => {
+    let versionDifference = "";
+
+    if (latestVersionSemver?.major !== versionSemver?.major) {
+        versionDifference = " major";
+    } else if (latestVersionSemver?.minor !== versionSemver?.minor) {
+        versionDifference = " minor";
+    }
+
+    console.log(
+        "=".repeat(80)
+    );
+
+    console.log(
+        `\n📣 There is a newer${versionDifference} version (${latestVersionSemver?.raw}) available (current version: ${versionSemver?.raw})\n`
+    );
+
+    console.log(
+        "=".repeat(80)
+    );
+}
