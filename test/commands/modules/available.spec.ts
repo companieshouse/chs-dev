79eee7f91c9dc08b00e09e012178c2251271107c
Joel Clemence <15887464+JoelClemence@users.noreply.github.com>
2024-07-04 15:44:07 +0100

    Add a json flag to info commands so the output can be programatically interrogated (#57)

--- test/commands/modules/available.spec.ts
+++ test/commands/modules/available.spec.ts
@@ -2,6 +2,7 @@ import { expect, jest } from "@jest/globals";
 import Available from "../../../src/commands/modules/available";
 import { modules } from "../../utils/data";
 import { Config } from "@oclif/core";
+import { parse } from "path";
 
 jest.mock("../../../src/state/inventory", () => {
     return {
@@ -16,6 +17,8 @@ jest.mock("../../../src/state/inventory", () => {
 describe("modules available", () => {
 
     let logMock;
+    let logJsonMock;
+    let parseMock;
     let availableModules: Available;
 
     beforeEach(() => {
@@ -24,10 +27,23 @@ describe("modules available", () => {
         availableModules = new Available([], {
             cacheDir: "./cache-dir"
         } as Config);
+
         logMock = jest.spyOn(availableModules, "log");
+
+        // @ts-expect-error
+        logJsonMock = jest.spyOn(availableModules, "logJson");
+
+        // @ts-expect-error
+        parseMock = jest.spyOn(availableModules, "parse");
     });
 
     it("logs all modules available", async () => {
+        parseMock.mockResolvedValue({
+            flags: {
+                json: false
+            }
+        });
+
         await availableModules.run();
 
         expect(logMock).toHaveBeenCalledTimes(modules.length + 1);
@@ -36,5 +52,23 @@ describe("modules available", () => {
         for (const { name } of modules) {
             expect(logMock).toHaveBeenCalledWith(` - ${name}`);
         }
+
+        expect(logJsonMock).not.toHaveBeenCalled();
+    });
+
+    it("can output json", async () => {
+        parseMock.mockResolvedValue({
+            flags: {
+                json: true
+            }
+        });
+
+        await availableModules.run();
+
+        expect(logJsonMock).toHaveBeenCalledWith({
+            modules: modules.map(({ name }) => name)
+        });
+
+        expect(logMock).not.toHaveBeenCalled();
     });
 });
