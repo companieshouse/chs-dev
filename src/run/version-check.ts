1764f42927a64b5ccf881424bc7c628298f35a6b
JoelClemence <jclemence@companieshouse.gov.uk>
2024-07-16 11:26:37 +0100

    Add Check for being on VPN (or accessing internal resources) to init
    * Some actions require connecting to internal resources only available over the VPN
      or internally
    * Refactor init hook extracting the version checking to a separate class

---
+++ src/run/version-check.ts
@@ -0,0 +1,139 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import Config from "../model/Config.js";
+import { diff, satisfies } from "semver";
+import { getLatestReleaseVersion } from "../helpers/latest-release.js";
+import { join } from "path";
+import { ThresholdUnit, timeWithinThreshold } from "../helpers/time-within-threshold.js";
+
+type VersionCheckLogger = {
+    warn: (msg: string) => void;
+    info: (msg: string) => void;
+}
+
+type CreateArguments = {
+    dataDirectory: string;
+    runThresholdDays: number;
+    logger: VersionCheckLogger;
+    config: Config;
+}
+
+/**
+ * A class which checks the version of the project and logs out any
+ * inconsistencies to latest version or project version
+ */
+export class VersionCheck {
+
+    // eslint-disable-next-line no-useless-constructor
+    private constructor (private readonly dataDirectory: string,
+        private readonly runThresholdDays: number,
+        private readonly logger: VersionCheckLogger,
+        private readonly config: Config
+    ) {
+
+    }
+
+    /**
+     * Runs version check checking the supplied version is approprate for the
+     * project and also whether a more recent version is available
+     * @param applicationVersion version to check
+     * @returns Promise representing outcome - resolves to undefined when
+     * successful and rejects when there is an error
+     */
+    async run (applicationVersion: string) {
+        const isEnvVarSet = (envVarName: string) => envVarName in process.env;
+
+        if (!isEnvVarSet("CHS_DEV_NO_PROJECT_VERSION_MISMATCH_WARNING") &&
+            !this.isSuitableVersionForProject(applicationVersion)) {
+
+            this.logUnsuitableVersion(applicationVersion, this.config.versionSpecification as string);
+
+            return;
+        }
+
+        const runTimeFile = join(this.dataDirectory, "last-version-run-time");
+        const executionTime = Date.now();
+
+        if (!timeWithinThreshold(runTimeFile, executionTime, this.runThresholdDays, ThresholdUnit.DAYS)) {
+            await this.checkCurrentVersionOfCliIsLatest(applicationVersion);
+
+            writeFileSync(
+                runTimeFile,
+                new Date(executionTime).toISOString()
+            );
+        }
+
+    }
+
+    /**
+     * Creates a VersionCheck object ensuring the dataDir is present when it is
+     * absent
+     * @param argumentsObject - containing the properties to create the
+     * VersionCheck object
+     * @returns new VersionCheck object
+     */
+    static create (
+        {
+            dataDirectory,
+            runThresholdDays,
+            logger,
+            config
+        }: CreateArguments
+    ): VersionCheck {
+        if (!existsSync(dataDirectory)) {
+            mkdirSync(dataDirectory, { recursive: true });
+        }
+
+        return new VersionCheck(dataDirectory, runThresholdDays, logger, config);
+    }
+
+    private logUnsuitableVersion (actualVersion: string, requiredVersionSpecification: string) {
+        const sepLine = "-".repeat(80);
+        this.logger.warn(
+            `WARNING: this version: ${actualVersion} does not meet the project requirements: ${requiredVersionSpecification}
+        
+Run:
+
+\tchs-dev sync
+
+to update chs-dev to a suitable version
+${sepLine}
+
+`
+        );
+    };
+
+    private isSuitableVersionForProject (applicationVersion: string): boolean {
+        return !this.config.versionSpecification || satisfies(applicationVersion, this.config.versionSpecification); ;
+    }
+
+    private async checkCurrentVersionOfCliIsLatest (applicationVersion) {
+        const latestVersion = await getLatestReleaseVersion();
+
+        const semverDifference = diff(latestVersion, applicationVersion);
+
+        if (semverDifference !== null) {
+            this.logVersionDifference(latestVersion, applicationVersion, semverDifference);
+        }
+    };
+
+    private logVersionDifference (latestVersion: string, currentVersion: string, semverDifference: string) {
+        const versionDifference: string = semverDifference === "major" || semverDifference === "minor"
+            ? ` ${semverDifference}`
+            : "";
+
+        const sepLine = "=".repeat(80);
+        const versionOutOfDateMessage = `📣 There is a newer${versionDifference} version (${latestVersion}) available (current version: ${currentVersion})
+    
+Run:
+
+\tchs-dev sync
+
+to update to latest version
+`;
+
+        this.logger.info(
+            `${sepLine}\n\n${versionOutOfDateMessage}\n\n${sepLine}`
+        );
+    };
+
+}
---
+++ test/run/version-check.spec.ts
@@ -0,0 +1,277 @@
+import { expect, jest } from "@jest/globals";
+import Config from "../../src/model/Config";
+import { VersionCheck } from "../../src/run/version-check";
+import { getLatestReleaseVersion as getLatestReleaseVersionMock } from "../../src/helpers/latest-release";
+import { timeWithinThreshold as timeWithinThresholdMock } from "../../src/helpers/time-within-threshold";
+import fs from "fs";
+import { join } from "path";
+
+jest.mock("../../src/helpers/latest-release");
+jest.mock("../../src/helpers/time-within-threshold");
+
+describe("VersionCheck", () => {
+
+    const projectConfig: Config = {
+        env: {},
+        projectPath: "./docker-project",
+        projectName: "docker-project",
+        authenticatedRepositories: [],
+        versionSpecification: ">=1.0.0 <2.0.0"
+    };
+    const loggerMock = {
+        warn: jest.fn(),
+        info: jest.fn()
+    };
+    const runThresholdDays = 14;
+
+    const existsSyncSpy = jest.spyOn(fs, "existsSync");
+    const mkdirSyncSpy = jest.spyOn(fs, "mkdirSync");
+    const writeFileSyncSpy = jest.spyOn(fs, "writeFileSync");
+    const readFileSyncSpy = jest.spyOn(fs, "readFileSync");
+
+    const dataDir: string = "data/";
+
+    const version = "1.1.1";
+
+    const differentVersionTestCases = [
+        ["1.1.2", `📣 There is a newer version (1.1.2) available (current version: ${version})`],
+        ["1.2.0", `📣 There is a newer minor version (1.2.0) available (current version: ${version})`],
+        ["1.2.1", `📣 There is a newer minor version (1.2.1) available (current version: ${version})`],
+        ["2.0.0", `📣 There is a newer major version (2.0.0) available (current version: ${version})`],
+        ["2.0.1", `📣 There is a newer major version (2.0.1) available (current version: ${version})`],
+        ["2.1.0", `📣 There is a newer major version (2.1.0) available (current version: ${version})`]
+    ];
+
+    describe("create", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+
+            mkdirSyncSpy.mockImplementation((_, __) => { });
+        });
+
+        it("checks if data directory exists", () => {
+            VersionCheck.create({
+                dataDirectory: dataDir,
+                logger: loggerMock,
+                config: projectConfig,
+                runThresholdDays
+            });
+
+            expect(existsSyncSpy).toHaveBeenCalledWith(dataDir);
+        });
+
+        it("creates data directory when does not exist already", () => {
+            existsSyncSpy.mockReturnValue(false);
+
+            VersionCheck.create({
+                dataDirectory: dataDir,
+                logger: loggerMock,
+                config: projectConfig,
+                runThresholdDays
+            });
+
+            expect(mkdirSyncSpy).toHaveBeenCalledWith(dataDir, { recursive: true });
+        });
+
+        it("does not create data directory when already present", () => {
+            existsSyncSpy.mockReturnValue(true);
+
+            VersionCheck.create({
+                dataDirectory: dataDir,
+                logger: loggerMock,
+                config: projectConfig,
+                runThresholdDays
+            });
+
+            expect(mkdirSyncSpy).not.toHaveBeenCalled();
+        });
+
+        it("returns a VersionCheck object", () => {
+
+            existsSyncSpy.mockReturnValue(true);
+
+            const result = VersionCheck.create({
+                dataDirectory: dataDir,
+                logger: loggerMock,
+                config: projectConfig,
+                runThresholdDays
+            });
+
+            expect(result).toBeInstanceOf(VersionCheck);
+        });
+    });
+
+    describe("run", () => {
+        let versionCheck: VersionCheck;
+
+        describe("first run through", () => {
+            beforeEach(() => {
+                jest.resetAllMocks();
+
+                existsSyncSpy.mockReturnValueOnce(true);
+
+                versionCheck = VersionCheck.create({
+                    dataDirectory: dataDir,
+                    logger: loggerMock,
+                    config: projectConfig,
+                    runThresholdDays
+                });
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                delete process.env.CHS_DEV_NO_PROJECT_VERSION_MISMATCH_WARNING;
+
+                writeFileSyncSpy.mockImplementation((_, __) => { });
+
+                existsSyncSpy.mockReturnValue(false);
+            });
+
+            it("fetches the latest version", async () => {
+
+                await versionCheck.run(version);
+
+                expect(getLatestReleaseVersionMock).toHaveBeenCalled();
+            });
+
+            for (const [newerVersion, expectedMessage] of differentVersionTestCases) {
+                it(`displays correct message when there is newer version: ${newerVersion}`, async () => {
+                    // @ts-expect-error
+                    getLatestReleaseVersionMock.mockResolvedValue(newerVersion);
+
+                    await versionCheck.run(version);
+
+                    expect(loggerMock.info.mock.calls).toMatchSnapshot();
+                });
+            }
+
+            it("displays correct message when the installed version not meeting project", async () => {
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue("0.9.23");
+
+                await versionCheck.run("0.9.23");
+
+                expect(loggerMock.warn.mock.calls).toMatchSnapshot();
+            });
+        });
+
+        describe("subsequent run through", () => {
+
+            const lastRunThrough = "2024-01-01T00:00:00.000Z";
+
+            let currentTime: Date;
+
+            beforeEach(() => {
+                jest.resetAllMocks();
+
+                existsSyncSpy.mockReturnValue(true);
+
+                versionCheck = VersionCheck.create({
+                    dataDirectory: dataDir,
+                    logger: loggerMock,
+                    config: projectConfig,
+                    runThresholdDays
+                });
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                readFileSyncSpy.mockReturnValue(
+                    Buffer.from(
+                        lastRunThrough
+                    )
+                );
+
+                const dateNowMock = jest.fn();
+                dateNowMock.mockImplementation(() => currentTime.getTime());
+
+                // @ts-expect-error
+                Date.now = dateNowMock;
+
+                delete process.env.CHS_DEV_NO_PROJECT_VERSION_MISMATCH_WARNING;
+
+                writeFileSyncSpy.mockImplementation((_, __) => { });
+
+            });
+
+            it("checks version when after time passed", async () => {
+                currentTime = new Date(2024, 0, 16, 0, 0, 0, 0);
+
+                // @ts-expect-error
+                timeWithinThresholdMock.mockReturnValue(false);
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                await versionCheck.run(version);
+
+                expect(getLatestReleaseVersionMock).toHaveBeenCalled();
+            });
+
+            it("writes out current time when has checked version", async () => {
+                currentTime = new Date(2024, 0, 16, 0, 0, 0, 0);
+
+                // @ts-expect-error
+                timeWithinThresholdMock.mockReturnValue(false);
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                await versionCheck.run(version);
+
+                expect(writeFileSyncSpy).toHaveBeenCalledWith(
+                    join(dataDir, "last-version-run-time"),
+                    "2024-01-16T00:00:00.000Z"
+                );
+            });
+
+            it("does not check version when time passed not passed", async () => {
+                // @ts-expect-error
+                timeWithinThresholdMock.mockReturnValue(true);
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                await versionCheck.run(version);
+
+                expect(getLatestReleaseVersionMock).not.toHaveBeenCalled();
+            });
+
+            it("checks version when CHS_DEV_CHECK_VERSION is set", async () => {
+                process.env.CHS_DEV_CHECK_VERSION = "1";
+
+                currentTime = new Date(2024, 0, 13, 0, 0, 0, 0);
+
+                // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue(version);
+
+                await versionCheck.run(version);
+
+                expect(getLatestReleaseVersionMock).toHaveBeenCalled();
+            });
+
+            it("displays correct message when the installed version not meeting project", async () => {
+            // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue("0.9.23");
+
+                await versionCheck.run("0.9.23");
+
+                expect(loggerMock.warn.mock.calls).toMatchSnapshot();
+            });
+
+            it("does not display message when the installed version not meeting project and env var set", async () => {
+            // @ts-expect-error
+                getLatestReleaseVersionMock.mockResolvedValue("0.9.23");
+
+                process.env.CHS_DEV_NO_PROJECT_VERSION_MISMATCH_WARNING = "true";
+
+                await versionCheck.run("0.9.23");
+
+                expect(loggerMock.warn).not.toHaveBeenCalled();
+            });
+
+        });
+
+    });
+
+});
