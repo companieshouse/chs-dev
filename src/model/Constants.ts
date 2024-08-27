7921ea104dc1a6538062b807b0cdd9bf4d41c931
JoelClemence <jclemence@companieshouse.gov.uk>
2024-08-27 16:29:39 +0100

    Append `secrets` property to builder services when service requires it
    * It is more secure to use Docker Compose secret's management offered by the
      secrets top level element and secrets array on a service
    * These secrets are mounted within the `/run/secrets/` directory within
      the container and are not viewable in the logs or Docker Console

---
+++ src/model/Constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Constant values for chs-dev projects
+ */
+export const CONSTANTS = {
+    /**
+     * The initial Docker Compose Spec which forms the basis of the generated
+     * Docker Compose specification defining the environment
+     */
+    BASE_DOCKER_COMPOSE_FILE: "services/infrastructure/docker-compose.yaml",
+
+    /**
+     * Value for a boolean label to be considered true
+     */
+    BOOLEAN_LABEL_TRUE_VALUE: "true"
+};
+
+export default CONSTANTS;
---
+++ test/generator/development/spec-assembly/builder-secrets-spec-assembly-functions.spec.ts
@@ -0,0 +1,194 @@
+import { expect, jest } from "@jest/globals";
+import fs from "fs";
+import { DockerComposeSpec } from "../../../../src/model/DockerComposeSpec";
+
+import { services } from "../../../utils/data";
+import builderSecretsSpecAssemblyFunction from "../../../../src/generator/development/spec-assembly/builder-secrets-spec-assembly-function";
+import yaml from "yaml";
+import { join } from "path";
+import CONSTANTS from "../../../../src/model/Constants";
+import Service from "../../../../src/model/Service";
+
+describe("builderSecretsSpecAssemblyFunction", () => {
+    const readFileSyncSpy = jest.spyOn(fs, "readFileSync");
+
+    let service: Service;
+    let developmentDockerComposeSpec: DockerComposeSpec;
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+
+        service = services[0];
+        developmentDockerComposeSpec = {
+            services: {
+                [`${service.name}-builder`]: {},
+                [service.name]: {}
+            }
+        };
+
+        delete service.metadata.secretsRequired;
+    });
+
+    it("reads in initial docker compose file", () => {
+        service.metadata.secretsRequired = "true";
+
+        readFileSyncSpy.mockReturnValue(Buffer.from(yaml.stringify({
+            secrets: {
+                "secret-one": {
+                    environment: "ENV_VAR_ONE"
+                },
+                "secret-two": {
+                    file: "./file.txt"
+                }
+            }
+        }), "utf-8"));
+
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(readFileSyncSpy).toHaveBeenCalledWith(
+            join("/home/testuser/docker-c", CONSTANTS.BASE_DOCKER_COMPOSE_FILE),
+            "utf8"
+        );
+    });
+
+    it("adds secrets to builder when service defines requiresSecrets", () => {
+        service.metadata.secretsRequired = "true";
+
+        readFileSyncSpy.mockReturnValue(Buffer.from(yaml.stringify({
+            secrets: {
+                "secret-one": {
+                    environment: "ENV_VAR_ONE"
+                },
+                "secret-two": {
+                    file: "./file.txt"
+                }
+            }
+        }), "utf-8"));
+
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(developmentDockerComposeSpec.services[`${service.name}-builder`].secrets)
+            .toEqual([
+                "secret-one",
+                "secret-two"
+            ]);
+    });
+
+    it("does not set secrets when there are no secrets in initial docker compose", () => {
+        service.metadata.secretsRequired = "true";
+
+        readFileSyncSpy.mockReturnValue(Buffer.from(yaml.stringify({ }), "utf-8"));
+
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(developmentDockerComposeSpec.services[`${service.name}-builder`].secrets).toBeUndefined();
+    });
+
+    it("does not set secrets on main service", () => {
+        service.metadata.secretsRequired = "true";
+
+        readFileSyncSpy.mockReturnValue(Buffer.from(yaml.stringify({
+            secrets: {
+                "secret-one": {
+                    environment: "ENV_VAR_ONE"
+                },
+                "secret-two": {
+                    file: "./file.txt"
+                }
+            }
+        }), "utf-8"));
+
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(developmentDockerComposeSpec.services[service.name].secrets).toBeUndefined();
+    });
+
+    it("does not set secrets when secretsRequired not set", () => {
+        readFileSyncSpy.mockReturnValue(Buffer.from(yaml.stringify({
+            secrets: {
+                "secret-one": {
+                    environment: "ENV_VAR_ONE"
+                },
+                "secret-two": {
+                    file: "./file.txt"
+                }
+            }
+        }), "utf-8"));
+
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(developmentDockerComposeSpec.services[`${service.name}-builder`].secrets).toBeUndefined();
+    });
+
+    it("does not read spec in when secrets not required", () => {
+        builderSecretsSpecAssemblyFunction(developmentDockerComposeSpec, {
+            service,
+            projectPath: "/home/testuser/docker-c",
+            serviceDockerComposeSpec: {
+                services: {}
+            },
+            builderDockerComposeSpec: {
+                builderSpec: "",
+                name: "",
+                version: ""
+            }
+        });
+
+        expect(readFileSyncSpy).not.toHaveBeenCalled();
+    });
+});
