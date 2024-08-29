26fec745125f4bf3d91be72a0d7d2b6c966f8865
JoelClemence <jclemence@companieshouse.gov.uk>
2024-08-29 17:27:46 +0100

    Services may have other attributes which should be copied across
    * CHIPS failed to run in development mode since the original spec created a volune
      which was missing now it didn't copy across the service

--- src/generator/development/spec-assembly/index.ts
+++ src/generator/development/spec-assembly/index.ts
@@ -4,6 +4,7 @@ import builderSecretsSpecAssemblyFunction from "./builder-secrets-spec-assembly-
 import builderSpecAssemblyFunction from "./builder-spec-assembly-function.js";
 import dependsOnSpecAssemblyFunction from "./depends-on-spec-assembly-function.js";
 import envFileSpecAssemblyFunction from "./env-file-assembly-function.js";
+import nonServiceTlaSpecAssemblyFunction from "./non-services-tla-spec-assembly-function.js";
 import { simpleSpecAssemblyFunctionFactory } from "./simple-spec-assembly-function.js";
 import { SpecAssemblyFunction, SpecAssemblyFunctionOptions } from "./spec-assembly-function.js";
 
@@ -14,7 +15,10 @@ const specFieldsWhichAreImmutable = [
     "image",
     "healthcheck",
     "ports",
-    "expose"
+    "expose",
+    "dns",
+    "dns_search",
+    "volumes"
 ];
 
 const specAssemblyFunctions: SpecAssemblyFunction[] = [
@@ -23,7 +27,8 @@ const specAssemblyFunctions: SpecAssemblyFunction[] = [
     ...specFieldsWhichAreImmutable.map(simpleSpecAssemblyFunctionFactory),
     buildArgsSpecAssemblyFunction,
     envFileSpecAssemblyFunction,
-    builderSecretsSpecAssemblyFunction
+    builderSecretsSpecAssemblyFunction,
+    nonServiceTlaSpecAssemblyFunction
 ];
 
 /**
