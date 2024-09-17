5d1dc9f54c1447d496f0c444f410e0bfe00e07ae
JoelClemence <jclemence@companieshouse.gov.uk>
2024-09-17 18:02:13 +0100

    Remove duplicate call to builderSecretsSpecAssemblyFunction

--- src/generator/development/spec-assembly/index.ts
+++ src/generator/development/spec-assembly/index.ts
@@ -24,7 +24,6 @@ const specAssemblyFunctions: SpecAssemblyFunction[] = [
     dependsOnSpecAssemblyFunction,
     buildArgsSpecAssemblyFunction,
     envFileSpecAssemblyFunction,
-    builderSecretsSpecAssemblyFunction,
     nonServiceTlaSpecAssemblyFunction,
     volumeSpecAssemblyFunction,
     builderSecretsSpecAssemblyFunction
