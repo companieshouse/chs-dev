10ed11d9c5d73f1ec1d2d45758b72499089b80e3
JoelClemence <jclemence@companieshouse.gov.uk>
2024-11-15 09:17:11 +0000

    Create TroubleshootReport utility which will produce a troubleshoot report
    * Create TroubleshootReport which will create a report of the current state of tthe environment
    * Add src/helpers/user-input.ts which provide user input helpers
    * Create report task which outputs the command that failed

---
+++ src/run/troubleshoot/report/identifying-command-output-troubleshoot-report-task.ts
@@ -0,0 +1,34 @@
+import { input } from "../../../helpers/user-input.js";
+
+/**
+ * An OutputTroubleshootReportTask which will prompt the user to supply the
+ * command that failed and write it to the troubleshooting context
+ */
+export class IdentifyingCommandOutputTroubleshootReportTask {
+    async run ({
+        context
+    }): Promise<string | undefined> {
+        let failingCommand: string;
+        while (true) {
+            failingCommand = await input(
+                "What command was it that failed (please be exact)?"
+            );
+
+            if (typeof failingCommand !== "undefined" && /\S/.test(failingCommand)) {
+                break;
+            }
+
+            console.error(
+                "Please enter a value for failing command."
+            );
+        }
+
+        context.append({
+            command: failingCommand
+        });
+
+        return undefined;
+    }
+}
+
+export default new IdentifyingCommandOutputTroubleshootReportTask();
