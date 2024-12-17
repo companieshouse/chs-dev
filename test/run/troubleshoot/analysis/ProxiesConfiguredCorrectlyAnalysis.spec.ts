e297ccb3bdd659045773d03b71bea1753f588b10
knite51 <aolubiyo@companieshouse.gov.uk>
2024-12-17 09:31:00 +0000

    fix process.env variable in test suite

--- test/run/troubleshoot/analysis/ProxiesConfiguredCorrectlyAnalysis.spec.ts
+++ test/run/troubleshoot/analysis/ProxiesConfiguredCorrectlyAnalysis.spec.ts
@@ -3,17 +3,26 @@ import { expect, jest } from "@jest/globals";
 import ProxiesConfiguredCorrectlyAnalysis from "../../../../src/run/troubleshoot/analysis/ProxiesConfiguredCorrectlyAnalysis";
 import { TroubleshootAnalysisTaskContext } from "../../../../src/run/troubleshoot/analysis/AnalysisTask";
 import AnalysisOutcome from "../../../../src/run/troubleshoot/analysis/AnalysisOutcome";
+import childProcess from "child_process";
+import { isOnVpn, isWebProxyHostSet } from "../../../../src/helpers/vpn-check";
 
 jest.mock("fs");
+jest.mock("../../../../src/helpers/vpn-check", () => ({
+    isWebProxyHostSet: jest.fn(),
+    isOnVpn: jest.fn()
+}));
 
 describe("ProxiesConfiguredCorrectlyAnalysis", () => {
     const mockDockerSettingsPath = "/mock/docker/settings-store.json";
-
     let analysis: ProxiesConfiguredCorrectlyAnalysis;
+    const WEB_PROXY_SUGGESTION = [
+        "Run echo $CH_PROXY_HOST to check web proxy value on device"
+    ];
 
-    beforeEach(() => {
+    beforeEach(async () => {
         analysis = new ProxiesConfiguredCorrectlyAnalysis();
         jest.resetAllMocks();
+
     });
 
     it("should return a successful outcome if no issues are found", async () => {
@@ -29,6 +38,9 @@ describe("ProxiesConfiguredCorrectlyAnalysis", () => {
             })
         );
 
+        (isWebProxyHostSet as jest.Mock).mockReturnValue(true);
+        (isOnVpn as jest.Mock).mockReturnValue(true);
+
         const context = {
             config: { dockerSettingsPath: mockDockerSettingsPath }
         } as TroubleshootAnalysisTaskContext;
@@ -39,12 +51,84 @@ describe("ProxiesConfiguredCorrectlyAnalysis", () => {
         expect(outcome.isSuccess()).toBe(true);
     });
 
+    it("should return an issue if CH_PROXY_HOST is not set", async () => {
+        process.env.HTTPS_PROXY = "http://proxy.example.com";
+
+        (fs.existsSync as jest.Mock).mockReturnValue(true);
+        (fs.readFileSync as jest.Mock).mockReturnValue(
+            JSON.stringify({
+                OverrideProxyHTTP: "http://proxy.example.com",
+                OverrideProxyHTTPS: "http://proxy.example.com",
+                ProxyHttpMode: "manual",
+                ProxyHTTPMode: "manual"
+            })
+        );
+
+        delete process.env.CH_PROXY_HOST;
+        (isWebProxyHostSet as jest.Mock).mockReturnValue(false);
+
+        const context = {
+            config: { dockerSettingsPath: mockDockerSettingsPath }
+        } as TroubleshootAnalysisTaskContext;
+
+        const outcome = await analysis.analyse(context);
+
+        expect(outcome).toBeInstanceOf(AnalysisOutcome);
+        expect(outcome.isSuccess()).toBe(false);
+        expect(outcome.issues).toEqual([
+            {
+                title: "CH_PROXY_HOST env not set",
+                description: "CH_PROXY_HOST value missing in env.",
+                suggestions: WEB_PROXY_SUGGESTION,
+                documentationLinks: []
+            }
+        ]);
+    });
+
+    it("should return an issue if CH_PROXY_HOST ping fails (not on VPN)", async () => {
+        process.env.HTTPS_PROXY = "http://proxy.example.com";
+        process.env.CH_PROXY_HOST = "websenseproxy.internal.ch";
+
+        (fs.existsSync as jest.Mock).mockReturnValue(true);
+        (fs.readFileSync as jest.Mock).mockReturnValue(
+            JSON.stringify({
+                OverrideProxyHTTP: "http://proxy.example.com",
+                OverrideProxyHTTPS: "http://proxy.example.com",
+                ProxyHttpMode: "manual",
+                ProxyHTTPMode: "manual"
+            })
+        );
+
+        (isWebProxyHostSet as jest.Mock).mockReturnValue(true);
+        (isOnVpn as jest.Mock).mockReturnValue(false);
+
+        const context = {
+            config: { dockerSettingsPath: mockDockerSettingsPath }
+        } as TroubleshootAnalysisTaskContext;
+
+        const outcome = await analysis.analyse(context);
+
+        expect(outcome).toBeInstanceOf(AnalysisOutcome);
+        expect(outcome.isSuccess()).toBe(false);
+        expect(outcome.issues).toEqual([
+            {
+                title: "CH_PROXY_HOST ping unsuccessful",
+                description: "Ping on webproxy not successful",
+                suggestions: WEB_PROXY_SUGGESTION,
+                documentationLinks: []
+            }
+        ]);
+    });
+
     it("should return a failed outcome if the Docker settings file is missing", async () => {
         (fs.existsSync as jest.Mock).mockReturnValue(false);
 
         const context = {
             config: { dockerSettingsPath: mockDockerSettingsPath }
         } as TroubleshootAnalysisTaskContext;
+
+        (isWebProxyHostSet as jest.Mock).mockReturnValue(true);
+        (isOnVpn as jest.Mock).mockReturnValue(true);
 
         const outcome = await analysis.analyse(context);
 
@@ -77,6 +161,9 @@ describe("ProxiesConfiguredCorrectlyAnalysis", () => {
             config: { dockerSettingsPath: mockDockerSettingsPath }
         } as TroubleshootAnalysisTaskContext;
 
+        (isWebProxyHostSet as jest.Mock).mockReturnValue(true);
+        (isOnVpn as jest.Mock).mockReturnValue(true);
+
         const outcome = await analysis.analyse(context);
 
         expect(outcome).toBeInstanceOf(AnalysisOutcome);
@@ -97,4 +184,5 @@ describe("ProxiesConfiguredCorrectlyAnalysis", () => {
             }
         ]);
     });
+
 });
