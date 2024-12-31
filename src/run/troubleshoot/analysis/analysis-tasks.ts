fdb2fb63832934af7ee457407cb3f6c38ae23c23
knite51 <aolubiyo@companieshouse.gov.uk>
2024-12-31 12:11:56 +0000

    add version analysis to analysis task

--- src/run/troubleshoot/analysis/analysis-tasks.ts
+++ src/run/troubleshoot/analysis/analysis-tasks.ts
@@ -3,12 +3,14 @@ import DockerMemoryAnalysis from "./DockerMemoryAnalysis.js";
 import ECRLoginAnalysis from "./ECRLoginAnalysis.js";
 import ProxiesConfiguredCorrectlyAnalysis from "./ProxiesConfiguredCorrectlyAnalysis.js";
 import ServicesInLiveUpdateConfiguredCorrectlyAnalysis from "./ServicesInLiveUpdateConfiguredCorrectlyAnalysis.js";
+import VersionAnalysis from "./VersionAnalysis.js";
 
 const analysisTasks: AnalysisTask[] = [
     new ServicesInLiveUpdateConfiguredCorrectlyAnalysis(),
     new ProxiesConfiguredCorrectlyAnalysis(),
     new DockerMemoryAnalysis(),
-    new ECRLoginAnalysis()
+    new ECRLoginAnalysis(),
+    new VersionAnalysis()
 ];
 
 export default analysisTasks;
