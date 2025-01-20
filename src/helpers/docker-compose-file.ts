dc504ff47808f52ca2d3a43dda481b4540df9fd1
knite51 <aolubiyo@companieshouse.gov.uk>
2025-01-20 08:30:23 +0000

    document function

--- src/helpers/docker-compose-file.ts
+++ src/helpers/docker-compose-file.ts
@@ -4,6 +4,12 @@ import yaml from "yaml";
 import CONSTANTS from "../model/Constants.js";
 import { DockerComposeSpec } from "../model/DockerComposeSpec.js";
 
+/**
+ * Get the initial compose file by path
+ *
+ * @param path - default compose file path location
+ * @returns a yaml format of the compose file
+ */
 export const getInitialDockerComposeFile = (path: string): DockerComposeSpec => {
     return yaml.parse(
         readFileSync(join(path, CONSTANTS.BASE_DOCKER_COMPOSE_FILE), "utf8").toString()
