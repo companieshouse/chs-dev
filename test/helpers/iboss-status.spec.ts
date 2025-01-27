e80ac85fe5cc7b5763bcdb604460ce8bf395c8b8
knite51 <aolubiyo@companieshouse.gov.uk>
2025-01-27 10:19:03 +0000

    refactor test using the data driven approach

--- test/helpers/iboss-status.spec.ts
+++ test/helpers/iboss-status.spec.ts
@@ -1,36 +1,18 @@
-import { expect, jest } from "@jest/globals";
+import { expect, jest, test } from "@jest/globals";
 import { isIbossEnabled } from "./../../src/helpers/iboss-status.js";
 
 describe("check the iboss flag is present", () => {
     afterEach(() => {
         delete process.env.CH_IBOSS_TRIAL;
     });
-    it("should return true when CH_IBOSS_TRIAL is set to 'true' (case insensitive)", () => {
-        process.env.CH_IBOSS_TRIAL = "true";
-        expect(isIbossEnabled()).toBe(true);
-    });
 
-    it("should return false when CH_IBOSS_TRIAL is set to 'false'", () => {
-        process.env.CH_IBOSS_TRIAL = "false";
+    test.each(["invalid", "undefined", "false"])("should return false when CH_IBOSS_TRIAL is %s", (ibossTrialEnvVar) => {
+        process.env.CH_IBOSS_TRIAL = ibossTrialEnvVar;
         expect(isIbossEnabled()).toBe(false);
     });
 
-    it("should return false when CH_IBOSS_TRIAL is undefined", () => {
-        expect(isIbossEnabled()).toBe(false);
-    });
-
-    it("should return false when CH_IBOSS_TRIAL is set to an invalid value", () => {
-        process.env.CH_IBOSS_TRIAL = "invalid";
-        expect(isIbossEnabled()).toBe(false);
-    });
-
-    it("should return true when CH_IBOSS_TRIAL is set to uppercase 'TRUE'", () => {
-        process.env.CH_IBOSS_TRIAL = "TRUE";
-        expect(isIbossEnabled()).toBe(true);
-    });
-
-    it("should return true when CH_IBOSS_TRIAL is set to mixed case 'TrUe'", () => {
-        process.env.CH_IBOSS_TRIAL = "TrUe";
+    test.each(["true", "TRUE", "TrUe"])("should return true when CH_IBOSS_TRIAL is set to %s", (ibossTrialEnvVar) => {
+        process.env.CH_IBOSS_TRIAL = ibossTrialEnvVar;
         expect(isIbossEnabled()).toBe(true);
     });
 
