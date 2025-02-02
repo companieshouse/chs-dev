ce41c4b7e063369b54d1adc4e1ade061a92fb646
knite51 <aolubiyo@companieshouse.gov.uk>
2025-02-02 00:38:06 +0000

    add general helper array related functions

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -17,3 +17,25 @@ export const isObjectFormat = (T: unknown): boolean => {
 export const isEmptyObject = (obj: Record<string, any>): boolean => {
     return Object.keys(obj).length === 0;
 };
+
+/**
+ * Checks if there is at least one common element between two arrays.
+ *
+ * @param {unknown[]} array1 - The first array to compare.
+ * @param {unknown[]} array2 - The second array to compare.
+ * @returns {boolean} - Returns `true` if any element in `array1` exists in `array2`, otherwise `false`.
+ */
+export const matchExistInArrays = (array1: unknown[], array2: unknown[]): boolean => {
+    return array1.some(item => array2.includes(item));
+};
+
+/**
+ * Finds and returns elements from the first array that do not exist in the second array.
+ *
+ * @param {unknown[]} array1 - The source array to check.
+ * @param {unknown[]} array2 - The reference array to compare against.
+ * @returns {any[]} - An array containing elements from `array1` that are not present in `array2`.
+ */
+export const findUniqueItems = (array1: unknown[], array2: unknown[]): any[] => {
+    return array1.filter(item => !array2.includes(item));
+};
