cd997378e12799c9c91325dc793a9b989d9839fc
knite51 <aolubiyo@companieshouse.gov.uk>
2025-02-04 14:27:25 +0000

    modify iboss env variable check
    - return a truthy value if the environment variable is set to anything other than 'false', '0', 'no' or an empty string.

--- src/helpers/iboss-status.ts
+++ src/helpers/iboss-status.ts
@@ -1,9 +1,9 @@
 /**
- * checks if the CH_IBOSS_TRIAL variable is set in the environment as a truthy value
+ * checks if the CH_IBOSS_TRIAL variable is set in the environment as a boolean value
  *
  * @returns boolean
  */
 export const isIbossEnabled: () => boolean = () => {
     const env = process.env.CH_IBOSS_TRIAL?.toLowerCase();
-    return !!env && env === "true";
+    return !!(env && env !== "false" && env !== "no" && env !== "0");
 };
