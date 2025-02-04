3291c61ffbbc746e135d04a38eabc70a8f1c7829
knite51 <aolubiyo@companieshouse.gov.uk>
2025-02-04 15:55:55 +0000

    remove double negative for improved clarity

--- src/helpers/iboss-status.ts
+++ src/helpers/iboss-status.ts
@@ -1,10 +1,12 @@
 /**
- * checks if the CH_IBOSS_TRIAL variable is set in the environment as a boolean value
+ * checks if the CH_IBOSS_TRIAL variable is set and not any of the invalid values
+ * `"false", "no", "0"`
  *
  * @returns boolean
  */
 export const isIbossEnabled: () => boolean = () => {
     const env = process.env.CH_IBOSS_TRIAL?.toLowerCase();
     const invalidValues = new Set(["false", "no", "0"]);
-    return !!(env && !invalidValues.has(env));
+    if (!env) return false;
+    return !invalidValues.has(env);
 };
