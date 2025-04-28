2798055db47820a4c1319c8e46948f9fdbbe7730
knite51 <aolubiyo@companieshouse.gov.uk>
2025-04-28 18:46:06 +0100

    refactor  test cases for more coverage
    - seperate the node services validators into it own module
    - update test cases

--- src/hooks/check-development-service-config.ts
+++ src/hooks/check-development-service-config.ts
@@ -1,13 +1,18 @@
 import { Hook } from "@oclif/core";
-import { existsSync, readFileSync } from "fs";
+import { existsSync } from "fs";
 import { join } from "path";
-import yaml from "yaml";
 import loadConfig from "../helpers/config-loader.js";
+import {
+    logDocumentationLink,
+    validateLabelForSubmodulesIntegration,
+    validateNodemonEntryContent,
+    validateNodemonJsonContent,
+    validateNodePackageJson
+} from "../helpers/development-mode-validators.js";
 import Service from "../model/Service.js";
 
 type ServicesByBuilder = { [builder: string]: Service[] };
 
-const DOCUMENTATION_LINKS = "troubleshooting-remedies/correctly-node-services-for-development-mode.md";
 // @ts-ignore
 export const hook: Hook<"check-development-service-config"> = async ({ servicesByBuilder, context }: { servicesByBuilder: ServicesByBuilder }) => {
     const projectPath = loadConfig().projectPath;
@@ -37,12 +42,12 @@ const checkNodeServiceConfig = (service: Service, projectPath: string, context) 
     const nodemonEntryFilePathServer = join(servicePath, "server/bin/nodemon-entry.ts");
 
     if (existsSync(servicePath)) {
-    // Validate submodule integration labels
+        // Validate submodule integration labels
         validateLabelForSubmodulesIntegration(servicePath, service, context);
 
         // Validate package.json file
         if (existsSync(packageJsonPath)) {
-            validatePackageJson(packageJsonPath, service.name, context);
+            validateNodePackageJson(packageJsonPath, service.name, context);
         } else {
             logMissingFile(context, service.name, "package.json");
             return;
@@ -69,109 +74,7 @@ const checkNodeServiceConfig = (service: Service, projectPath: string, context) 
     }
 };
 
-/**
- * Validates the presence of required labels for submodule integration
- * @param servicePath - Path to the local service directory.
- * @param service - Service object.
- * @param context - Context for logging messages.
- * @returns {void}
-*/
-const validateLabelForSubmodulesIntegration = (servicePath, service: Service, context) => {
-    const gitModulesPath = join(servicePath, ".gitmodules");
-    if (existsSync(gitModulesPath)) {
-        const dockerCompose = yaml.parse(readFileSync(service.source, "utf-8"));
-        const serviceConfig = dockerCompose.services?.[service.name];
-        const requiresSecretsLabel = serviceConfig?.labels?.find(label => label.startsWith("chs.local.builder.requiresSecrets"));
-
-        if (requiresSecretsLabel !== "chs.local.builder.requiresSecrets=true") {
-            context.warn(`Service ${service.name} is missing the label "chs.local.builder.requiresSecrets=true" in its docker-compose configuration as it depends on a submodule.\n`);
-            logDocumentationLink(context);
-        }
-    }
-};
-
-/**
- * Validates the package.json file for required scripts and dependencies
- * @param packageJsonPath - Path to the package.json file in the service.
- * @param serviceName - Service name.
- * @param context - Context for logging messages.
- * @returns {void}
-*/
-const validatePackageJson = (packageJsonPath: any, serviceName: string, context) => {
-    const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
-
-    const hasChsDevScript = packageJson.scripts?.["chs-dev"] === "nodemon --legacy-watch";
-    const hasNodemonInstalled = !!packageJson.devDependencies?.nodemon;
-    if (!hasChsDevScript) {
-        context.warn(`Service ${serviceName} is missing the "chs-dev" script or has incorrect value in package.json.\n`);
-        logDocumentationLink(context);
-    }
-
-    if (!hasNodemonInstalled) {
-        context.warn(`Service ${serviceName} is missing the nodemon package in its devDependencies.\n`);
-        logDocumentationLink(context);
-    }
-};
-
-/**
- * Validates the content of the nodemon entry file
- * @param actualNodemonEntryPath - Path to the nodemon-entry.ts file in the service.
- * @param serviceName - Service name.
- * @param context - Context for logging messages.
- * @returns {void}
-*/
-const validateNodemonEntryContent = (actualNodemonEntryPath, serviceName: string, context) => {
-    const actualFileContent = readFileSync(actualNodemonEntryPath, "utf-8");
-
-    const listenRegex = /\.listen\s*\(\s*PORT\s*,\s*\(\s*\)\s*=>\s*(\{[\s\S]*?\}|[^\n;]*?)\)/;
-    const logLineRegex = /console\.log\s*\(\s*`✅\s+Application Ready\. Running on port \$\{PORT\}`\s*\)\s*;?/;
-
-    const hasListen = listenRegex.test(actualFileContent);
-    const hasLog = logLineRegex.test(actualFileContent);
-
-    if (!hasListen || !hasLog) {
-        context.warn(`Service ${serviceName} nodemon entry file: '${actualNodemonEntryPath}' is missing the required listen event or log line.\n`);
-        logDocumentationLink(context);
-    }
-};
-
-/**
- * Validates the content of the nodemon.json configuration file
- * @param projectPath - Project root path.
- * @param actualNodemonConfigPath - Path to the actual nodemon.json file in the service.
- * @param serviceName - Service name.
- * @param context - Context for logging messages.
- * @returns {void}
-*/
-const validateNodemonJsonContent = (projectPath: string, actualNodemonConfigPath, serviceName: string, context) => {
-    const expectedConfigPath = join(projectPath, "local/builders/node/v3/bin/config/nodemon.json");
-
-    if (!existsSync(expectedConfigPath)) {
-        context.warn(`Expected nodemon configuration file is missing at ${expectedConfigPath}.\n`);
-        logDocumentationLink(context);
-        return;
-    }
-
-    const expectedConfig = JSON.parse(readFileSync(expectedConfigPath, "utf-8"));
-    const actualConfig = JSON.parse(readFileSync(actualNodemonConfigPath, "utf-8"));
-
-    if (JSON.stringify(expectedConfig.events) !== JSON.stringify(actualConfig.events) ||
-    !JSON.stringify(actualConfig.exec).includes("/bin/nodemon-entry.ts") ||
-    actualConfig.watch.length === 0) {
-        context.warn(`Service ${serviceName} has an incorrect nodemon.json configuration.\n`);
-        logDocumentationLink(context);
-    }
-};
-
 const logMissingFile = (context, serviceName: string, fileName: string) => {
     context.error(`Service ${serviceName} is missing ${fileName}.\n`);
     logDocumentationLink(context);
-};
-
-const logDocumentationLink = (context) => {
-    context.error(`Use as setup guide:- ${documentationLink(DOCUMENTATION_LINKS)}`);
-};
-
-const documentationLink = (documentationFileName: string) => {
-    return `https://www.github.com/companieshouse/chs-dev/blob/main/docs/${documentationFileName}`;
 };
