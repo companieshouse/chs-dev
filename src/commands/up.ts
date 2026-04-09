99c423738d10e1a08f66d06c8e708f8d5a6a9c1f
stefano basile <sbasile@companieshouse.gov.uk>
2026-04-09 09:13:35 +0100

    Add init script on "up" (#178)
    * Add init script on "up"
    
    * Dvop 4480 env flag (#177)
    
    * Add env flag to chs-dev for local env spinup
    
    * Add dynamicEnv to Config
    
    * add custom init functionality
    
    * bump minor version (releasing custom init functionality)
    
    ---------
    
    Co-authored-by: Ben Cullerton <49912713+bcullerton@users.noreply.github.com>

--- src/commands/up.ts
+++ src/commands/up.ts
@@ -1,5 +1,8 @@
 import confirm from "@inquirer/confirm";
 import { Command, Config, Flags, ux } from "@oclif/core";
+import { execSync } from "child_process";
+import fs from "fs";
+import { CONSTANTS } from "../model/Constants.js";
 
 import { basename } from "path";
 import loadConfig from "../helpers/config-loader.js";
@@ -142,6 +145,19 @@ export default class Up extends Command {
         ux.action.start("Ensuring all permanent repos are up to date");
         await this.permanentRepositories.ensureAllExistAndAreUpToDate();
         ux.action.stop();
+
+        let customInitOutput = "";
+        ux.action.start("Running init shell script");
+        try {
+            const customScript = `${this.chsDevConfig.projectPath}/${CONSTANTS.SHELL_CUSTOM_INIT_SCRIPT}`;
+            customInitOutput = fs.existsSync(customScript)
+                ? execSync(customScript, { encoding: "utf-8" })
+                : `Skipped init script ${customScript} as not present`;
+        } catch (err) {
+            this.log("Failed to execute init script: " + err);
+        }
+        ux.action.stop();
+        this.log(customInitOutput);
 
         ux.action.start(`Running chs-dev environment: ${basename(this.chsDevConfig.projectPath)}`);
         try {
